ad5335605d290cad95981f4a08c84d87eb08f0a4
Andrew da Silva <andrewairamdasilva@gmail.com>
2025-01-31 23:14:26 -0300

    fix(job-management): refactoring configuration module

--- src/job-management/job-management.module.ts
+++ src/job-management/job-management.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { JobController } from './controller/job.controller';
 import { JobManagementService } from './service/job-management.service';
 import { JobRepository } from './repository/job.repository';
+import { ConfigModule } from '@src/config/config.module';
+import { PrismaModule } from '@src/prisma/prisma.module';
 
 @Module({
+  imports: [ConfigModule.forRoot(), PrismaModule],
   controllers: [JobController],
   providers: [JobManagementService, JobRepository],
 })
