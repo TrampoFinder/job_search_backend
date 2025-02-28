7c66fe3e81da2b5e8c1e4eaca362372af3bf67f9
Andrew da Silva <andrewairamdasilva@gmail.com>
2025-02-27 22:35:54 -0300

    fix(Module): fix imports with alias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { JobManagementModule } from '@src/module/job-management/job-management.module';
-import { ReportManagementModule } from '@src/module/report-management/report-management.module';
-import { IdentityModule } from '@src/module/identity/identity.module';
+import { JobManagementModule } from '@jobManagementModule/job-management.module';
+import { ReportManagementModule } from '@reportManagementModule/report-management.module';
+import { IdentityModule } from '@identityModule/identity.module';
 
 @Module({
   imports: [IdentityModule, JobManagementModule, ReportManagementModule],
